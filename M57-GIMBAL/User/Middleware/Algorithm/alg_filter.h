3cc3bc70ab9aac26924b89a5f0932db2843f9f0c
ji096929 <127857437+ji096929@users.noreply.github.com>
2024-07-12 08:31:08 +0800

    Update alg_filter.h

--- M57-GIMBAL/User/Middleware/Algorithm/alg_filter.h
+++ M57-GIMBAL/User/Middleware/Algorithm/alg_filter.h
@@ -1,9 +1,6 @@
 /**
  * @file alg_filter.h
- * @author yssickjgd (1345578933@qq.com)
- * @brief 滤波器
- * @version 0.1
- * @date 2023-08-29 0.1 23赛季定稿
+ 
  * @date 2023-09-25 1.1 可自定义滤波器阶数
  *
  * @copyright Copyright (c) 2023
